571b1086f92ae63df3117c1a5fe654e0f8c1aa0d
nmsk6 <galogen28nmsk@gmail.com>
2023-10-21 01:23:37 +0300

    changed author class

--- dproject/myapp2/models.py
+++ dproject/myapp2/models.py
@@ -4,8 +4,11 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     email = models.EmailField()
-    biography =
-    birthday =
+    biography = models.TextField(blank=True)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return f'Name: {self.name},lastname: {self.lastname}, email: {self.email}, biography: {self.biography}, birthday: {self.birthday}'
 
 
     def __str__(self):
